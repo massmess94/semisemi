87be2a8ad3b1f1ac37918b8e014d0797088c4eeb
JeonByeongHyeon <iedl97@naver.com>
2023-06-19 15:35:23 +0900

    MemberVO 생성

---
+++ semi-Check-In-To-U-Project/src/main/java/org/jeongdab/tou/model/MemberVO.java
@@ -0,0 +1,28 @@
+package org.jeongdab.tou.model;
+
+public class MemberVO {
+	String id;
+	String password;
+	public MemberVO() {
+		super();
+		
+	}
+	public MemberVO(String id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
